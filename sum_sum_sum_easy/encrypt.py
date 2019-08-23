9d7977a64b292c446eed3036668d18a71a18234d
kurenaif <antyobido@gmail.com>
2019-08-23 22:27:12 +0900

    sum_sum_sum_easy

---
+++ sum_sum_sum_easy/encrypt.py
@@ -0,0 +1,21 @@
+#!/usr/loca/bin python3
+import typing
+from fractions import Fraction
+import random
+from flag import flag
+
+def encrypt(S, num):
+    cs = []
+    for c in S:
+        cs.append(ord(c))
+    
+    cs += cs
+    res = []
+    for i in range(len(S)):
+        res.append(sum(cs[i:i+num]))
+    return res
+
+m = flag
+c = encrypt(m, random.randrange(len(m)-1)+1)
+for i in range(len(c)):
+    print("C[{}]={}".format(i,c[i]))

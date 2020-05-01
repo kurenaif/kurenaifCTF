3977a36401644ae1c54bc695ca689265213b445d
kurenaif <antyobido@gmail.com>
2020-05-02 03:29:14 +0900

    rsa_basic

---
+++ rsa_basic/rsa.py
@@ -0,0 +1,23 @@
+from Crypto.Util.number import *
+from Crypto.Random import *
+import secret
+
+p = getRandomNBitInteger(512)
+while not isPrime(p):
+    p = getRandomNBitInteger(512)
+
+q = getRandomNBitInteger(512)
+while not isPrime(q):
+    q = getRandomNBitInteger(512)
+
+n = p * q
+e = 65537
+
+m = bytes_to_long(secret.flag.encode('utf-8')) # ここにフラグを数値に変換したものが入ります
+
+print("n = {}".format(n))
+print("p = {}".format(p))
+print("q = {}".format(q))
+print("e = {}".format(e))
+print("m = {}".format(m))
+print("c = {}".format(pow(m,e,n)))
